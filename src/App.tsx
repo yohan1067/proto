5d03c460f80e80d99e976965c51161da08567b10
yohan1067 <yohan1067@gmail.com>
2026-02-03 12:53:17 +0000

    Fix: react-router-dom 누락 및 불필요한 React import 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // Deployment check: 2026-02-03
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useTranslation } from 'react-i18next';
 import ChatMessage from './components/ChatMessage';
